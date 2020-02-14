c561722ad6c182960ddebac85287bc0cc5a0419e
Dylan Peck <dylanmpeck@gmail.com>
2020-02-13 17:12:20 -0800

    Added tunnel and post processing. Looks gnarly.

---
+++ Assets/Kvant/Tunnel/TunnelScroller.cs
@@ -0,0 +1,30 @@
+﻿//
+// Scroller script for Tunnel
+//
+using UnityEngine;
+
+namespace Kvant
+{
+    [RequireComponent(typeof(Tunnel))]
+    [AddComponentMenu("Kvant/Tunnel Scroller")]
+    public class TunnelScroller : MonoBehaviour
+    {
+        [SerializeField]
+        float _speed;
+
+        bool forwards = true;
+
+        public float speed {
+            get { return _speed; }
+            set { _speed = value; }
+        }
+
+        void Update()
+        {
+            if (!forwards)
+                GetComponent<Tunnel>().offset += _speed * Time.deltaTime;
+            else
+                GetComponent<Tunnel>().offset -= _speed * Time.deltaTime;
+        }
+    }
+}
